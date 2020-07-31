02287fd44667bcb91c01f8e1497f11f621ceeb43
mf5xs <55819948+mf5xs@users.noreply.github.com>
2020-07-31 12:57:37 -0400

    Add files via upload

---
+++ ELF codecave/payload.asm
@@ -0,0 +1,20 @@
+; writes "POC" to stdout
+; compiled with "nasm -f elf payload.asm -o payload.o;ld -m elf_i386 payload.o -o payload"
+
+
+section .text 
+	global _start
+
+_start:
+
+	; sys_write
+	mov	edx, 	5	; arg3 = len of string
+
+	push 	0x000a
+	push 	'POC'
+	mov 	ecx, 	esp	; arg2 = ptr to str
+
+	mov 	ebx, 	1	; arg1 = 1 = stdout 
+	mov 	eax, 	4	; sys_write syscall number
+	int 	0x80
+
